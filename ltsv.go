69c2279845dd654ba5ebeb8ebcd810594b2fff39
Songmu <y.songmu@gmail.com>
2016-07-10 02:52:40 +0900

    ltsv.Unmarshal

---
+++ ltsv.go
@@ -0,0 +1,158 @@
+package ltsv
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type Unmarshaler interface {
+	UnmarshalLTSV([]byte) error
+}
+
+// Loaderror is an error type for Load()
+type LoadError map[string]error
+
+func (m LoadError) Error() string {
+	if len(m) == 0 {
+		return "(no error)"
+	}
+
+	ee := make([]string, 0, len(m))
+	for name, err := range m {
+		ee = append(ee, fmt.Sprintf("field %q: %s", name, err))
+	}
+
+	return strings.Join(ee, "\n")
+}
+
+// OfField returns the error correspoinding to a given field
+func (m LoadError) OfField(name string) error {
+	return m[name]
+}
+
+// An UnmarshalTypeError describes a LTSV value that was
+// not appropriate for a value of a specific Go type.
+type UnmarshalTypeError struct {
+	Value string
+	Type  reflect.Type
+}
+
+func (e *UnmarshalTypeError) Error() string {
+	return "ltsv: cannot unmarshal " + e.Value + " into Go value of type " + e.Type.String()
+}
+
+type ltsvMap map[string]string
+
+func data2map(data []byte) (ltsvMap, error) {
+	d := string(data)
+	fields := strings.Split(d, "\t")
+	l := ltsvMap{}
+	for _, v := range fields {
+		kv := strings.SplitN(strings.TrimSpace(v), ":", 2)
+		if len(kv) != 2 {
+			return nil, fmt.Errorf("not a ltsv: %s", d)
+		}
+		l[strings.TrimSpace(kv[0])] = strings.TrimSpace(kv[1])
+	}
+	return l, nil
+}
+
+func Unmarshal(data []byte, v interface{}) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr {
+		return fmt.Errorf("not a pointer: %v", v)
+	}
+
+	rv = rv.Elem()
+	if rv.Kind() != reflect.Struct && rv.Kind() != reflect.Map {
+		return fmt.Errorf("not a pointer to a struct/map: %v", v)
+	}
+
+	l, err := data2map(data)
+	if err != nil {
+		return err
+	}
+	if rv.Kind() == reflect.Map {
+		kt := rv.Type().Key()
+		vt := rv.Type().Elem()
+		if kt.Kind() != reflect.String || vt.Kind() != reflect.String {
+			return fmt.Errorf("not a map[string]string")
+		}
+		for k, v := range l {
+			kv := reflect.ValueOf(k).Convert(kt)
+			vv := reflect.ValueOf(v).Convert(vt)
+			rv.SetMapIndex(kv, vv)
+		}
+		return nil
+	}
+
+	t := rv.Type()
+	errs := LoadError{}
+	for i := 0; i < t.NumField(); i++ {
+		ft := t.Field(i)
+		fv := rv.Field(i)
+
+		if !fv.CanSet() {
+			continue
+		}
+
+		tag := ft.Tag.Get("ltsv")
+		if tag == "" {
+			continue
+		}
+
+		tags := strings.Split(tag, ",")
+		key := tags[0]
+		if key == "-" {
+			continue
+		}
+		s, ok := l[key]
+		if !ok {
+			continue
+		}
+
+		switch fv.Kind() {
+		case reflect.String:
+			fv.SetString(s)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			i, err := strconv.ParseInt(s, 10, 64)
+			if err != nil || fv.OverflowInt(i) {
+				errs[ft.Name] = &UnmarshalTypeError{"number " + s, fv.Type()}
+				continue
+			}
+			fv.SetInt(i)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			i, err := strconv.ParseUint(s, 10, 64)
+			if err != nil || fv.OverflowUint(i) {
+				errs[ft.Name] = &UnmarshalTypeError{"number " + s, fv.Type()}
+				continue
+			}
+			fv.SetUint(i)
+		case reflect.Float32, reflect.Float64:
+			n, err := strconv.ParseFloat(s, fv.Type().Bits())
+			if err != nil || fv.OverflowFloat(n) {
+				errs[ft.Name] = &UnmarshalTypeError{"number " + s, fv.Type()}
+				continue
+			}
+			fv.SetFloat(n)
+		case reflect.Interface:
+			if u, ok := fv.Interface().(Unmarshaler); ok {
+				err := u.UnmarshalLTSV([]byte(s))
+				if err != nil {
+					errs[ft.Name] = err
+				}
+				continue
+			}
+			fallthrough
+		default:
+			errs[ft.Name] = &UnmarshalTypeError{s, fv.Type()}
+		}
+	}
+
+	if len(errs) < 1 {
+		return nil
+	}
+	return errs
+}
---
+++ ltsv_test.go
@@ -0,0 +1,52 @@
+package ltsv
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestData2Map(t *testing.T) {
+	l, _ := data2map([]byte("hoge: fuga\tpiyo: piyo"))
+
+	expect := ltsvMap{
+		"hoge": "fuga",
+		"piyo": "piyo",
+	}
+	if !reflect.DeepEqual(l, expect) {
+		t.Errorf("result of data2map not expected: %#v", l)
+	}
+
+	_, err := data2map([]byte("hoge"))
+	if err.Error() != "not a ltsv: hoge" {
+		t.Errorf("something went wrong")
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	m := make(map[string]string)
+	Unmarshal([]byte("hoge: fuga\tpiyo: piyo"), &m)
+
+	expect := map[string]string{
+		"hoge": "fuga",
+		"piyo": "piyo",
+	}
+	if !reflect.DeepEqual(m, expect) {
+		t.Errorf("result of data2map not expected: %#v", m)
+	}
+
+	type ss struct {
+		User   string  `ltsv:"user"`
+		Age    uint8   `ltsv:"age"`
+		Height float64 `ltsv:"height"`
+	}
+	s := &ss{}
+	Unmarshal([]byte("user:songmu\tage:36\theight:169.1"), s)
+	expect2 := &ss{
+		User:   "songmu",
+		Age:    36,
+		Height: 169.1,
+	}
+	if !reflect.DeepEqual(s, expect2) {
+		t.Errorf("result of data2map not expected: %#v", s)
+	}
+}
